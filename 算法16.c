d431da3f5ca5ee097edd6d51d3d168427481b5ca
Joey <37296285+Joeyhana@users.noreply.github.com>
2020-03-08 15:51:06 +0800

    PAT B1011

---
+++ 算法16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    long long a,b,da,db,pa=0 ,pb=0;
+    // ‰»Î
+    scanf("%lld %lld %lld %lld",&a,&da,&b,&db);
+    while(a!=0){//±È¿˙ a
+        if(a%10==da) pa=pa*10+da;
+        a=a/10;
+    }
+    while(b!=0){
+        if(b%10==db) pb=pb*10+db;
+        b=b/10;
+    }
+    printf("%lld\n",pa+pb);
+
+    return 0;
+}
