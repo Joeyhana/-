d57cc4d08265eeccbb78d019136d80360a566fad
Joey <37296285+Joeyhana@users.noreply.github.com>
2020-03-30 15:37:36 +0800

    PAT A1077

--- A1077.cpp
+++ A1077.cpp
@@ -1,37 +1,45 @@
 #include <cstdio>
 #include <cstring>
+#include<iostream>
+using namespace std;
+
 
 int n,minlen=256,ans=0;
-char s[100][256];
+char str[105][300];
 
 int main(){
     scanf("%d",&n);
     getchar();
     for(int i=0;i<n;i++){
-        scanf("%s",s[i]);
-        int len=strlen(s[i]);
+        //解决pat 不能用gets的方法
+        cin.getline(str[i],300);
+        int len=strlen(str[i]);
         if(len<minlen) minlen=len;
+
         for(int j=0;j<len/2;j++){
-            char temp=s[i][j];
-            s[i][j]=s[i][len-1-j];
-            s[i][len-1-j]=temp;
+            char temp=str[i][j];
+            str[i][j]=str[i][len-1-j];
+            str[i][len-1-j]=temp;
         }
     }
+
     for(int i=0;i<minlen;i++){
-        char c=s[0][i];
+        char c=str[0][i];
         bool same=true;
+
         for(int j=1;j<n;j++){
-            if(c!=s[j][i]){
+            if(c!=str[j][i]){
                 same=false;
                 break;
             }
         }
-        if(same) ans++;
+        if(same==true) ans++;
         else break;
     }
-    if(ans){
+
+    if(ans!=0){
         for(int i=ans-1;i>=0;i--){
-            printf("%c",s[0][i]);
+            printf("%c",str[0][i]);
         }
     }else{
         printf("nai");
